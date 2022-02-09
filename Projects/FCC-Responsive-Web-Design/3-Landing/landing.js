bee755ea87fc344d8ea6a815ee2af1e8dc493642
bholmes999 <bholmes@harding.edu>
2022-02-09 16:55:58 -0600

    commit

--- Projects/FCC-Responsive-Web-Design/3-Landing/landing.js
+++ Projects/FCC-Responsive-Web-Design/3-Landing/landing.js
@@ -1,3 +1,5 @@
+/* Burger button menu dropdown */
+
 let burgerButton = document.getElementById('burger-btn')
 let navMenu = document.getElementById('nav-menu')
 let navMenuActive = false
@@ -9,9 +11,33 @@ burgerButton.addEventListener('click', function(){
         navMenuActive = false
     } else {
         navMenu.style.visibility = 'visible'
-        navMenu.style.height = '100%'
+        navMenu.style.height = '100vh'
         navMenuActive = true
     }
 })
 
-// navMenu.style.backgroundColor = 'red';
+/* Modal form sign-up */
+
+let header = document.getElementById('header')
+let modalButtons = document.querySelectorAll('.action-btn')
+let modalDiv = document.getElementById('signup-modal-container')
+let modalActive = false
+
+
+modalButtons.forEach(element => {
+    element.addEventListener('click', function(){
+        if(modalActive) {
+            modalDiv.style.visibility = 'hidden'
+            modalDiv.style.height = '0'
+            header.style.visibility = 'visible'
+            modalActive = false
+
+        } else {
+            modalDiv.style.visibility = 'visible'
+            modalDiv.style.height = '100%'
+            header.style.visibility = 'hidden'
+            modalActive = true
+        }
+    })    
+});
+
